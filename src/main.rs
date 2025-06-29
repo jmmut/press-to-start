1073b54d8a1e3d7c1c9eafb0588bc9d705ae125c
jose miguel mut <jomutlo@gmail.com>
2025-06-30 00:15:35 +0200

    add circle interaction and minor refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use juquad::draw::{draw_rect, draw_rect_lines};
 use juquad::input::input_macroquad::InputMacroquad;
 use juquad::input::input_trait::InputTrait;
-use juquad::PixelPosition;
 use juquad::widgets::anchor::{Anchor, Horizontal, Layout, Vertical};
 use juquad::widgets::anchorer::Anchorer;
 use juquad::widgets::button::Button;
 use juquad::widgets::text::TextRect;
 use juquad::widgets::{Interaction, Widget};
 use juquad::widgets::{StateStyle, Style};
+use juquad::PixelPosition;
 use macroquad::miniquad::date::now;
 use macroquad::prelude::*;
 
+pub const FORCE_RANGE_PIXELS: f32 = 100.0;
 pub const LIGHT_GREEN: Color = Color::new(0.7, 0.85, 0.7, 1.0);
 pub const TOOLTIP_BACKGROUND: Color = LIGHTGRAY;
 
@@ -59,6 +60,11 @@ pub const STAGE_TORUS_ENABLED: bool = true;
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    let button = stage_torus().await;
+    stage_prison(button).await;
+}
+
+async fn stage_torus() -> Button {
     let (sw, sh) = (screen_width(), screen_height());
     let anchor = Anchor::center(sw * 0.5, sh * 0.5);
     let mut text = "Start";
@@ -80,6 +86,9 @@ async fn main() {
         }
         if is_key_pressed(KeyCode::Escape) {
             break;
+        }
+        if is_key_pressed(KeyCode::Space) {
+            println!("mouse pos: {:?}", mouse_position());
         }
         clear_background(LIGHTGRAY);
         if debug {
@@ -123,6 +132,12 @@ async fn main() {
         if move_button {
             button.reanchor(Anchor::center_v(new_pos));
         }
+        let diff = (mouse_pos - button_center).length();
+        let alpha =
+            ((2.0 * FORCE_RANGE_PIXELS - diff) / (2.0 * FORCE_RANGE_PIXELS) / 3.0).clamp(0.0, 0.5);
+        // 2*RANGE => 0, 0.5*range => 1
+        let halo_color = with_alpha(GRAY, alpha);
+        draw_halo(new_pos.x, new_pos.y, diff, halo_color);
         let size = button.rect().size();
         let mut extra_buttons = Vec::new();
         if button.rect().x < 0.0 {
@@ -157,7 +172,6 @@ async fn main() {
             let extra = Anchor::center_v(new_pos + vec2(sw, -sh)).get_rect(size);
             extra_buttons.push(extra);
         }
-        // move_inside(button.rect_mut(), Rect::new(0.0, 0.0, sw, sh));
         if debug {
             let mut anchorer = Anchorer::new_pos(layout, vec2(0.0, 0.0), 0.0);
 
@@ -176,6 +190,8 @@ async fn main() {
                 text_rect.render_default(&STYLE.at_rest);
             }
         }
+
+        // check if any alternative buttons have interactions
         let original = button.rect();
         let mut interaction = button.interact();
         for extra in &extra_buttons {
@@ -185,25 +201,30 @@ async fn main() {
             *button.rect_mut() = *extra;
             interaction = button.interact();
         }
+        // any extra button with interaction will become the main button
         let rect_interacted = if interaction != Interaction::None {
             Some(button.rect())
         } else {
             None
         };
+
+        // render main and extra buttons
         *button.rect_mut() = original;
         render_button(&button);
         for extra in &extra_buttons {
             *button.rect_mut() = *extra;
             render_button(&button);
         }
+
+        // print dialogue if triggered
         if let Some(start_dialogue_ts) = start_dialogue {
             let current_ts = now();
             if current_ts - start_dialogue_ts > DIALOG_DELAY_SECONDS {
                 start_dialogue = None;
-                dialog_index += 1;
-                if dialog_index == dialogs.len() {
+                if dialog_index == dialogs.len() - 1 {
                     break;
                 }
+                dialog_index = (dialog_index + 1).min(dialogs.len() - 1);
             } else {
                 let anchor = create_tooltip_anchor(sw, sh, button.rect());
                 render_tooltip(dialogs[dialog_index], anchor);
@@ -217,15 +238,23 @@ async fn main() {
             }
         }
 
+        // start dialogue with interacted
         *button.rect_mut() = original;
         if let Some(interacted) = rect_interacted {
-            if start_dialogue.is_none() {
+            if start_dialogue.is_some() {
+                let prev_dialog_index = dialog_index;
+                dialog_index = (dialog_index + 1).min(dialogs.len() - 1);
+                if prev_dialog_index != dialog_index {
+                    start_dialogue = Some(now());
+                }
+            } else {
                 start_dialogue = Some(now());
             }
             *button.rect_mut() = interacted;
         } else {
             let screen = Rect::new(0.0, 0.0, sw, sh);
             if screen.intersect(button.rect()).is_none() {
+                // no interaction and main button went out, so choose an extra as main
                 let mut max_area = 0.0;
                 for extra in extra_buttons {
                     if let Some(intersection) = extra.intersect(screen) {
@@ -238,6 +267,8 @@ async fn main() {
                 }
             }
         }
+
+        // should not happen
         if interaction.is_clicked() {
             text = "Start again";
             button = new_button(text, anchor);
@@ -255,13 +286,26 @@ async fn main() {
         }
         next_frame().await;
     }
+    button
+}
 
+pub async fn stage_prison(mut button: Button) {
     let dialogs = STAGE_PRISON_DIALOGS;
+    let mut dialog_index = 0;
 
     let fake_mouse_origin = Vec2::from(mouse_position());
     let input_grabbed: Box<dyn InputTrait> = Box::new(GrabbedMouseInput::new(fake_mouse_origin));
-    println!("mouse pos: {:?}, corrected: {:?}", mouse_position(), input_grabbed.mouse_position());
-    set_cursor_grab(true);
+    println!(
+        "mouse pos: {:?}, corrected: {:?}",
+        mouse_position(),
+        input_grabbed.mouse_position()
+    );
+    // set_cursor_grab(true);
+    println!(
+        "mouse pos after grab: {:?}, corrected: {:?}",
+        mouse_position(),
+        input_grabbed.mouse_position()
+    );
 
     let stage_2_start_ts = now();
     loop {
@@ -270,7 +314,11 @@ async fn main() {
             break;
         }
         if is_key_pressed(KeyCode::Space) {
-            println!("mouse pos: {:?}, corrected: {:?}", mouse_position(), input_grabbed.mouse_position());
+            println!(
+                "mouse pos: {:?}, corrected: {:?}",
+                mouse_position(),
+                input_grabbed.mouse_position()
+            );
         }
         let (sw, sh) = (screen_width(), screen_height());
         let screen_rect = Rect::new(1.0, 1.0, sw - 2.0, sh - 2.0);
@@ -325,13 +373,24 @@ async fn main() {
         next_frame().await;
     }
 }
+
+fn with_alpha(base: Color, alpha: f32) -> Color {
+    Color::new(base.r, base.g, base.b, alpha)
+}
+pub fn draw_halo(x: f32, y: f32, r: f32, color: Color) {
+    draw_poly(x, y, 40, r, 0., color);
+}
+
 pub struct GrabbedMouseInput {
     inner: Box<dyn InputTrait>,
     mouse_origin: Vec2,
 }
 impl GrabbedMouseInput {
     pub fn new(mouse_origin: Vec2) -> Self {
-        Self { inner: Box::new(InputMacroquad), mouse_origin }
+        Self {
+            inner: Box::new(InputMacroquad),
+            mouse_origin,
+        }
     }
 }
 impl InputTrait for GrabbedMouseInput {
@@ -364,7 +423,10 @@ impl InputTrait for GrabbedMouseInput {
     }
 
     fn clone(&self) -> Box<dyn InputTrait> {
-        Box::new(GrabbedMouseInput{inner: self.inner.clone(), mouse_origin: self.mouse_origin})
+        Box::new(GrabbedMouseInput {
+            inner: self.inner.clone(),
+            mouse_origin: self.mouse_origin,
+        })
     }
 }
 fn animate_pos_to(button: &mut Button, target_pos: Vec2) {
@@ -386,7 +448,7 @@ fn create_tooltip_anchor(sw: f32, sh: f32, next_to_this: Rect) -> Anchor {
 }
 
 fn compute_force(mouse_pos: Vec2, button_center: Vec2) -> Vec2 {
-    let range = 100.0;
+    let range = FORCE_RANGE_PIXELS;
     let diff = button_center - mouse_pos;
     let diff_unit = diff.normalize_or_zero();
     let magnitude = diff.length();
@@ -414,7 +476,14 @@ pub fn new_button(text: &str, anchor: Anchor) -> Button {
 }
 pub fn new_button_grabbed(text: &str, anchor: Anchor, input: &Box<dyn InputTrait>) -> Button {
     let input_clone = (*input).clone();
-    Button::new_generic(text, anchor, FONT_SIZE, None, macroquad::prelude::measure_text, input_clone)
+    Button::new_generic(
+        text,
+        anchor,
+        FONT_SIZE,
+        None,
+        macroquad::prelude::measure_text,
+        input_clone,
+    )
 }
 pub fn render_button(button: &Button) {
     button.render_default(&STYLE);
